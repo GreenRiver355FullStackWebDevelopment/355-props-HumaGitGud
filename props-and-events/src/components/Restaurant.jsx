311e3bec17da3df99c87ba3f7947923c0b0f2bae
HumaGitGud <khomidov.huma@student.greenriver.edu>
2025-04-08 11:51:05 -0700

    Made significant progress

--- props-and-events/src/components/Restaurant.jsx
+++ props-and-events/src/components/Restaurant.jsx
@@ -1,8 +1,10 @@
-function Restaurant() {
+function Restaurant(props) {
 
   return (
     <div className="restaurant">
-  
+      <p>{props.name}</p>
+      <p>{props.rating}</p>
+      <img src={props.restaurant.image} alt={props.name} />
     </div>
   );
 }
